98f0cb4cf1557d6859260995b606760724998baa
riyaz.shaik <riyaz.shaik@GGKU5DELL1131>
2020-11-17 23:40:50 +0530

    Integrated Monaco editor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
-
+import { Component } from "@angular/core";
+import { DiffEditorModel } from "ngx-monaco-editor";
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'DiffChecker';
+  originalText: any;
+  changedText: any;
+  differenceText: any;
+  editorOptions = {theme: 'vs', language: 'text/plain'};
+  originalModel: DiffEditorModel = {
+    code: "",
+    language: "text/plain",
+  };
+  displayDiffChecker: boolean = false;
+
+  modifiedModel: DiffEditorModel = {
+    code: "",
+    language: "text/plain",
+  };
+
+  checkDifference() {
+    this.originalModel = { ...this.originalModel, code: this.originalText };
+    this.modifiedModel = { ...this.originalModel, code: this.changedText };
+    this.displayDiffChecker = true;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { MonacoEditorModule } from "ngx-monaco-editor";
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    MonacoEditorModule.forRoot(),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
